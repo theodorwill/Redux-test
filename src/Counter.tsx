b3633f8848e8969bad3b902e6e7c238ce914974e
Theodor Will <theodor.will@gmail.com>
2022-12-21 00:52:09 +0100

    added redux to counter

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,11 +1,16 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increase, decrease } from './store/counterSlice';
 
 const Counter = () => {
-    const [ count, setCount ] = useState(0);
+
+    const dispatch = useDispatch();
+    const count = useSelector((state: any) => state.counter.value);
+
     return <>
         <p>Count: {count}</p>
-        <button onClick={() => setCount(count-1)}>-1</button>
-        <button onClick={() => setCount(count+1)}>+1</button>
+        <button onClick={() => dispatch(decrease(1))}>-</button>
+        <button onClick={() => dispatch(increase(1))}>+</button>
     </>
 }
 export default Counter;
